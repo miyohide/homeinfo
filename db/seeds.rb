a92e85a564d051ab3ab3b22896a2c6d6bc577c86
miyohide <miyohide@gmail.com>
2016-04-13 18:27:19 +0900

    rake db:migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do
+  srand(Time.now.to_i)
+  date_and_time = Time.now.strftime("%Y/%m/%d %H:%M:%S.%3N")
+  h = Humidity.create(humidity: sprintf("%.2f", rand(1.0..90.0)))
+  t = Temperature.create(temperature: sprintf("%.2f", rand(10.0..40.0)))
+  HomeinfoDatum.create(dateandtime: date_and_time,
+                       temperature_id: t.id,
+                       humidity_id: h.id)
+end
