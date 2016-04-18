a4b0f942d464ef6b25e979c440c358f5b19faff8
miyohide <miyohide@gmail.com>
2016-04-18 23:07:11 +0900

    fix 'not show graphe'

--- app/controllers/homeinfos_controller.rb
+++ app/controllers/homeinfos_controller.rb
@@ -1,7 +1,7 @@
 class HomeinfosController < ApplicationController
   def show
     homeinfo_data = HomeinfoDatum.all.includes(:humidity, :temperature)
-    @humidity = homeinfo_data.map { |item| [item.dateandtime, item.humidity.humidity] }
-    @temperature = homeinfo_data.map { |item| [item.dateandtime, item.temperature.temperature] }
+    @humidity = homeinfo_data.map { |item| [Time.parse(item.dateandtime), item.humidity.humidity] }
+    @temperature = homeinfo_data.map { |item| [Time.parse(item.dateandtime), item.temperature.temperature] }
   end
 end
