3429dc962930e9fd6ad862dbaa237300fe2ca0ac
miyohide <miyohide@gmail.com>
2016-04-20 22:22:56 +0900

    change rand initialize timing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
+srand(Time.now.to_i)
+
 100.times do
-  srand(Time.now.to_i)
   date_and_time = Time.now.strftime("%Y/%m/%d %H:%M:%S.%3N")
   h = Humidity.create(humidity: sprintf("%.2f", rand(1.0..90.0)))
   t = Temperature.create(temperature: sprintf("%.2f", rand(10.0..40.0)))
