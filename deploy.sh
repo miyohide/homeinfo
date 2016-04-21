3e31f80b2c89206ef858718d7e13d4d451b4b786
miyohide <miyohide@gmail.com>
2016-04-21 22:37:19 +0900

    delete db:seed

--- deploy.sh
+++ deploy.sh
@@ -123,7 +123,6 @@ echo DB migration
 pushd "$DEPLOYMENT_TARGET"
 
 $JRUBY_EXE -S vendor/bundle/bin/rake db:migrate RAILS_ENV=production
-$JRUBY_EXE -S vendor/bundle/bin/rake db:seed RAILS_ENV=production
 
 popd
 ##################################################################################################################################
